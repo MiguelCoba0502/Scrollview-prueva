4e348291feb180302b95003068fb44344104eaaa
MiguelCoba0502 <miguel_8090@hotmail.com>
2024-09-12 21:15:57 -0600

    Initial Commit

---
+++ Scrollview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scrollview
+//
+//  Created by Eduardo Coba on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scrollview/ScrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollviewApp.swift
+//  Scrollview
+//
+//  Created by Eduardo Coba on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
